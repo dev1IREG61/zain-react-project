8c931ed9e64c7658bddde073c0cbceed8924ae65
Mohsin-Javed48 <mohsinjavedpc@example.com>
2025-08-03 12:41:26 +0500

    Updated App.jsx and added deploy.yml

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       {showPopup && (
         <div className="bg-white rounded-2xl shadow-2xl p-6 text-center animate-fade-in">
           <h2 className="text-2xl font-bold text-purple-600 mb-2">Welcome</h2>
-          <p className="text-lg text-gray-700">Zain Khani</p>
+          <p className="text-lg text-gray-700">lahore</p>
           <button
             onClick={() => setShowPopup(false)}
             className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
